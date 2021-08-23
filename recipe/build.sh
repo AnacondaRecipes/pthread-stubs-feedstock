f8fcdfb2266e2eaf5b7775698f35d8f2832da577
Kai Tietz <ktietz@anaconda.com>
2021-08-23 10:50:34 +0200

    add osx-arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Get an updated config.sub and config.guess
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* .
+
 set -e
 IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
 
